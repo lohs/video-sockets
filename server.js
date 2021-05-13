911856d393afbb742ea6815c6c1f1c21e5d4af47
lohs <55119191+lohs@users.noreply.github.com>
2021-05-13 15:13:59 -0400

    changed port to 5001 and added comments

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 const express = require("express");
 const app = express();
 
@@ -19,15 +19,20 @@ app.get("/", (req, res) => {
   res.send("video webRTC socket server running");
 });
 
+// CREATE SOCKET CONNECTION
 io.on("connection", (socket) => {
+  
+  // SEND SOCKET ID TO CLIENT
   socket.emit("self", socket.id);
 
+  // CLIENT DISCONNECTED
   socket.on("disconnect", () => {
     socket.broadcast.emit("callEnded");
   });
 
+  // GET DATA FROM CALLER
   socket.on("callUser", (data) => {
-    console.log("Calling user!");
+    // SEND TO CALLEE
     io.to(data.userToCall).emit("callUser", {
       signal: data.signalData,
       from: data.from,
@@ -35,8 +40,9 @@ io.on("connection", (socket) => {
     });
   });
 
+  // GET DATA FROM CALLEE
   socket.on("answerCall", (data) => {
-    console.log("Accepting call!");
+    // SEND DATA TO CALLER
     io.to(data.to).emit("callAccepted", data.signal);
   });
 });
