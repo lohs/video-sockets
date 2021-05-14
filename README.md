0312d2f03bfa68b393a116dc26e59035a12d246d
lohs <55119191+lohs@users.noreply.github.com>
2021-05-14 17:10:51 -0400

    created readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Signaling Server for 499/HabitTracker
+
+## Deployed to: https://video-sockets.herokuapp.com/
+
+### [HabitTracker Main Repo](https://github.com/cs499-ht/499)
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const port = process.env.PORT || 5001;
 const express = require("express");
 const app = express();
-
 const http = require("http");
 const server = http.createServer(app);
 
